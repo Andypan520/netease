287cba509d912b985b3083ec2e48dfa5c5631ade
Andypan520 <Andypan520@yeah.net>
2018-01-07 17:42:17 +0800

    update FileUtil

--- cheetah/src/main/java/common/FileUtil.java
+++ cheetah/src/main/java/common/FileUtil.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,6 +22,7 @@ public class FileUtil {
 
     /**
      * 获取某个目录下的子文件名
+     *
      * @param dirName
      * @return
      * @throws IOException
@@ -35,6 +37,7 @@ public class FileUtil {
 
     /**
      * 读取某个文件
+     *
      * @param fileName
      * @return
      */
@@ -53,18 +56,25 @@ public class FileUtil {
      *
      * @param root
      * @param maxDepth
-     * @param fileNameMatch
+     * @param containContent
      * @throws IOException
      */
-    public static String findVagueFileNameWithDepth(String root, int maxDepth, String fileNameMatch) throws IOException {
+    public static String findVagueFileNameWithDepth(String root, int maxDepth, String containContent) throws IOException {
         Path start = Paths.get(root);
         try (Stream<Path> stream = Files.find(start, maxDepth, (path, attr) ->
-                String.valueOf(path).endsWith(fileNameMatch))) {
+                String.valueOf(path).endsWith(containContent))) {
             String joined = stream
                     .sorted()
                     .map(String::valueOf)
                     .collect(Collectors.joining("; "));
             return joined;
+        }
+    }
+
+    private static void write(String file, String content) throws IOException {
+        Path path = Paths.get(file);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            writer.write(content);
         }
     }
 }
