d9dec387b40fcd26862792c3d6a7d01ef8e180ca
pandechuan <pandechuan@corp.netease.com>
2018-01-08 16:34:02 +0800

    add test

---
+++ cheetah/src/test/java/Test.java
@@ -0,0 +1,11 @@
+import com.google.common.annotations.VisibleForTesting;
+
+/**
+ * Created by pandechuan on 2018/1/8.
+ */
+public class Test {
+    public static void main(String[] args){
+
+    }
+
+}
