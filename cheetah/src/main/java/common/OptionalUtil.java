894a88e292194ee4177325139b1b6a2b05b73642
pandechuan <pandechuan@corp.netease.com>
2018-01-16 15:47:08 +0800

    add test

--- cheetah/src/main/java/common/OptionalUtil.java
+++ cheetah/src/main/java/common/OptionalUtil.java
@@ -1,28 +1,54 @@
 package common;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
- * Created by pandechuan on 2018/1/6.
+ * Created by pandechuan on 2018/1/5.
  */
 public class OptionalUtil {
     /**
      * 防止取值NPE方法
      */
-    public static <T> Optional<T> resolve(Supplier<T> resolver) {
+    public static <T> Optional<T> get(Supplier<T> supplier) {
         try {
-            T result = resolver.get();
+            T result = supplier.get();
             return Optional.of(result);
         } catch (NullPointerException e) {
             return Optional.empty();
         }
     }
 
+    public static Optional<Integer> stringToInt(String s) {
+        try {
+            return Optional.of(Integer.parseInt(s));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<Long> stringToLong(String s) {
+
+        try {
+            return Optional.of(Long.parseLong(s));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+
     public static void main(String[] args) {
         Integer a = null;
-        Optional<Integer> integerOptional = OptionalUtil.resolve(() -> a);
+        Optional<Integer> integerOptional = OptionalUtil.get(() -> a);
         System.out.println(integerOptional.isPresent());
         System.out.println(integerOptional.toString());
+
+        List<String> list = Lists.newArrayList("1265", "f", " 5 ","89");
+       List<Integer> intList =  list.stream().map(OptionalUtil::stringToInt).filter(e -> e.isPresent()).map(e -> e.get()).collect(Collectors.toList());
+      intList.forEach(System.out::println);
     }
 }
